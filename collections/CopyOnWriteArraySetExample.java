9a335e8d951b13057cb09a01674d2b4a88a6f621
sunnyraj <mrsj5714@gmail.com>
2026-08-19 20:58:02 +0530

    java collections, constructors, exceptionalhandling, cocurrent, filehandling

---
+++ collections/CopyOnWriteArraySetExample.java
@@ -0,0 +1,31 @@
+package collections;
+
+import java.util.concurrent.*;
+import java.util.*;
+
+public class CopyOnWriteArraySetExample extends Thread {
+
+	static CopyOnWriteArraySet l = new CopyOnWriteArraySet();
+	
+	public void run() {
+		l.add("D");
+	}
+	public static void main(String[] args) {
+		l.add("A");
+		l.add("B");
+		l.add("C");
+		
+		// Child Thread
+		CopyOnWriteArraySetExample t = new CopyOnWriteArraySetExample();
+		t.start();
+		
+		try {
+			Thread.sleep(2000);
+			
+		} catch (InterruptedException e) {
+			System.out.println("Child is Going to add element");
+		}
+		
+		System.out.println(l);	
+	}
+}
